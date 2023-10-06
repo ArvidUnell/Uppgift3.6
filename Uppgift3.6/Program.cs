08740529e6ac3a5df09838158d20e60b7096cf2d
arun06001 <arun06001@E-PW0234KE>
2023-10-06 10:26:07 +0200

    Add project files.

---
+++ Uppgift3.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Uppgift3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt förnamn och sedan ditt efternamn med ett mellanslag emellan");
+            string svar = Console.ReadLine();
+
+            byte mellanIndex = (byte)svar.IndexOf(' ');
+            string förnamn = svar[..mellanIndex];
+            string efternamn = svar[(mellanIndex+1)..];
+
+            if (förnamn.CompareTo(efternamn) < 0)
+            {
+                Console.WriteLine($"Ditt förnamn {förnamn} kommer före ditt efternamn {efternamn} i alfabetisk ordning.");
+            }
+            else if (förnamn.CompareTo(efternamn) > 0)
+            {
+                Console.WriteLine($"Ditt efternamn {efternamn} kommer före ditt förnamn {förnamn} i alfabetisk ordning.");
+            }
+            else
+            {
+                Console.WriteLine("Du har samma förnamn och efternamn.");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
